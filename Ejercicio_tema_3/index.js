70e1684bc433257f508c3c45d1146eb4c8152a90
Edison <clanuzumaki22@gmail.com>
2022-04-11 13:01:16 -0500

    Ejercicio 1 tema 3

---
+++ Ejercicio_tema_3/index.js
@@ -0,0 +1,8 @@
+for(let num = 1; num <= 100; num++) {
+    if(num % 2 == 0) {
+        console.log("El numero " + num + " es par");
+    }
+    else {
+        console.log("El numero " + num + " es impar");
+    }
+}

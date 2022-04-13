401c2bf7c477bd2aac8e3c11cd8e84c812b70992
Edison <clanuzumaki22@gmail.com>
2022-04-12 20:32:26 -0500

    Ejercicio 1 tema 4

---
+++ Ejercicios_tema_4/index.js
@@ -0,0 +1,12 @@
+//Ejercicio 1
+
+let str = "El perro de mi vecina es muy molesto";
+console.log(str);
+
+let str2 = str.replace("vecina", "vecino");
+console.log(str2);
+
+//--------------------------------------------------------
+
+//Ejercicio 2
+

1bed81416c75bddcacd14a7819dab13de5bc7f8f
GitEdy2 <clanuzumaki22@gmail.com>
2022-06-07 18:27:31 -0500

    Creacion de archivo js con tipos de variables.

--- Ejercicio_tema_2/index.js
+++ Ejercicio_tema_2/index.js
@@ -1,9 +1,21 @@
-let a = "Una cadena de texto";
+const name = "Edison Proaño";
+console.log(typeof name);
 
-function funCadena() {
-    let a = "Otra cadena de caracteres";
-    console.log(a);
+const edad = 32;
+console.log(typeof edad);
+
+const eresDesarrollador = true;
+console.log(typeof eresDesarrollador);
+
+const fechaNacimiento = new Date("November 19 1990");
+console.log(fechaNacimiento);
+console.log(typeof fechaNacimiento);
+
+const libroFavorito = {
+  titulo: "Steve Jobs: Biografia",
+  autor: "WALTER ISAACSON",
+  fecha: "24 de Octubre del 2011",
+  url: "https://www.amazon.com/-/es/Walter-Isaacson/dp/030795028X"
 }
 
-funCadena();
-console.log(a);
+console.log(typeof libroFavorito);

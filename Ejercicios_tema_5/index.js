95fbf3380b98fa1caa61a1ebc078e4353cc8419a
GitEdy2 <clanuzumaki22@gmail.com>
2022-06-07 22:47:44 -0500

    Creacion de archivo js para numeros.

--- Ejercicios_tema_5/index.js
+++ Ejercicios_tema_5/index.js
@@ -1,3 +1,8 @@
-for (let i = 0; i < 10; i++) {
-    console.log(Math.round(Math.random()*20));   
-}
+const altura_cm = 163;
+const altura_m = 1.63;
+const peso_kg = 70.1;
+
+const altura_redondeo = Math.ceil(altura_m);
+const peso_redondeo = Math.floor(peso_kg);
+
+const numMaxIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;

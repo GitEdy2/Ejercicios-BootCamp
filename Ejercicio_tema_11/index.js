85d6c51dbb017e708e86d7a043145c81961e5642
GitEdy2 <clanuzumaki22@gmail.com>
2022-06-22 20:39:15 -0500

    Creacion de fichero js poo

---
+++ Ejercicio_tema_11/index.js
@@ -0,0 +1,17 @@
+class Estudiante {
+    constructor(nombre, asignaturas) {
+        this.nombre = "Edison";
+        this.asignaturas = ["Javascript", "HTML", "CSS"];
+    }
+
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+}
+
+const estudiante = new Estudiante();
+
+console.log(estudiante.obtenDatos());
